a9f698a0bcd2666a694a5948e2b89603924f2497
Umar Farouk <yasmarfaq@yahoo.com>
2024-04-18 12:41:02 +0100

    first commit

---
+++ my-github-repositories/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import ReposList from './component/GithubPortfolio';
+
+function App() {
+  return (
+    <div>
+      <h1>My GitHub Repositories Portfolio</h1>
+      <ReposList />
+    </div>
+  );
+}
+
+export default App;
